3760c4a4c026e1bc624cf649545ae36ffe3adf87
Fabian Choque <fabianchoque28@gmail.com>
2023-03-04 10:40:24 -0300

    configuracion de material UI

---
+++ themes/light-theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles';
+
+export const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '##1E1E1E'
+    },
+    secondary: {
+      main: '#3A64D8'
+    }  
+  }
+});
